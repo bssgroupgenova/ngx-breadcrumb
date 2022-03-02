a4deea0485ed63d876ffb74c1561891e11ca28eb
ildoc <il_doc@protonmail.com>
2022-03-02 14:12:11 +0100

    updated

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 
 /** Exporting the public API **/
 
-export { NgxBreadcrumbModule as Ng5BreadcrumbModule } from './src/app/components/breadcrumb/breadcrumb.module';
+export { NgxBreadcrumbModule as NgxBreadcrumbModule } from './src/app/components/breadcrumb/breadcrumb.module';
 export { BreadcrumbComponent } from './src/app/components/breadcrumb/breadcrumb.component';
 export { BreadcrumbService } from './src/app/components/breadcrumb/breadcrumb.service';
